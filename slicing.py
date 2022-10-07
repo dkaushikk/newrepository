b5fcd40567160a24c2e8aa00cb1a95bef5b1a49f
dkaushikk <kaushik.kanna3@gmail.com>
2022-10-07 14:33:08 +0530

    slicing is committed

---
+++ slicing.py
@@ -0,0 +1,14 @@
+d = "hello"
+print(d[0])
+print(d[0:len(d):1])
+print(d[0::1])
+print(d[0::])
+print(d[:])
+print(d[::])
+
+print(d[-5:-1])
+print(d[0:-1])
+print(d[-5:4])
+
+print(d[-1::-1])  # reverse of the string
+print(d[4::-1])
